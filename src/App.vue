a5d6260ec5a2a573f1fc9ef354abea06f2220990
haji-shab2 <itx.sameer.59@gmail.com>
2025-11-16 15:31:00 +0500

    first

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>'{{store.FullName }}' and rollNumber is{{store.rollNo }}</h1>
+
+</template>
+<script setup>
+import {  ref } from 'vue';
+import { useUserStore } from './store/user';
+const store=useUserStore()
+</script>               
